0b6fadd7aaf752d90251784e56d1186e81171533
ManojGSanningammanavar <manojsanningmmanavar@gmail.com>
2026-06-29 17:57:46 +0530

    Time: 5 ms (82.39%) | Memory: 77.3 MB (48.47%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,12 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        // 1.take left and right
-        // 2.check if value of left+1 is greater than left 
-        // 3.check if value of right-1 is greater than right
-        // 4.if left is grater than right return right**;
-        // 5. else return left**;  
-
         int left =0, right = height.length -1;
         int max = 0;
         while(left < right){
